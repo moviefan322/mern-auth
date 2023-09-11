300a76e0a926d4dc5a4c10f71b9edf41b8c69c70
moviefan322 <philman202@yahoo.com>
2023-09-11 18:13:46 -0400

    writes custom error handlers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,11 @@
+import asyncHandler from "express-async-handler";
+
 // @desc    Auth user/set token
 // @route   POST /api/users/login
 // @access Public
 
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth user" });
-};
+});
 
 export { authUser };
