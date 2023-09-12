ad287d14eaab65cddd5d26e907871140c49c1015
moviefan322 <philman202@yahoo.com>
2023-09-12 12:19:40 -0400

    adds RTKQuery boilerplate and login mutation

--- client/src/store.tsx
+++ client/src/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer },
-  middleware: (getDefaulteMiddleware) => getDefaulteMiddleware(),
+  reducer: { auth: authReducer, [apiSlice.reducerPath]: apiSlice.reducer },
+  middleware: (getDefaulteMiddleware) =>
+    getDefaulteMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
