b4db31dd152e85de2f05b978906ba7512f70486a
moviefan322 <philman202@yahoo.com>
2023-09-11 21:23:38 -0400

    writes logic for register user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 // @desc    Auth user/set token
 // @route   POST /api/users/login
@@ -13,7 +14,23 @@ const authUser = asyncHandler(async (req, res) => {
 // @access  Public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register user" });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({ _id: user._id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc    Logout user
